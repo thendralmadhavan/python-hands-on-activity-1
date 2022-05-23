268bb6644ca189996316f5f608a5a3195857f040
thendralmadhavan <106053817+thendralmadhavan@users.noreply.github.com>
2022-05-23 11:47:36 +0530

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,51 @@
+'''print("Hello world!")'''
+
+'''a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+sum=a+b
+print("Sum of the numbers is", sum)'''
+
+'''import math
+print(math.sqrt(36))'''
+
+'''num=int(input("enter a number: "))
+sqrt=num**(1/2)
+print("square root of the number is",sqrt)'''
+
+'''b=int(input("enter the number:"))
+h=int(input("enter the number:"))
+area =1/2*b*h
+print("The area of triangle is",area)'''
+
+'''celcius=float(input("enter the celcius:"))
+F=(1.8*celcius)+32
+print(round(F,1))'''
+
+'''print("My name is Thendral\nI am a b.tech graduate\nMy hobby is painting")'''
+
+'''import random
+print (random.randint(1,20))'''
+
+'''import math
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+c=int(input("enter c:"))
+d=(b**2)-(4*a*c)
+sol1=(-b+math.sqrt(d))/(2*a)
+sol2=(-b-math.sqrt(d))/(2*a)
+print("The solutions are",sol1,"and",sol2)'''
+
+'''a=int(input("enter  a:"))
+b=int(input("enter b:"))
+c=a
+a=b
+b=c
+print("The value of a after swapping is",a)
+print("The value of b after swapping is",b)'''
+
+'''kilometer=float(input("enter value in km:"))
+miles=kilometer*0.621371
+print("The given km is equal to",round(miles,2),"miles")'''
+
+
+
